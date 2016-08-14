359434b4a6bb4a8eebd1b67f67ef22fdc81dde95
natsanity <christmastreee04@gmail.com>
2016-08-14 18:11:48 +0800

    Initial commit

---
+++ app/src/main/java/com/example/nati/anew/AppConfig.java
@@ -0,0 +1,12 @@
+package com.example.nati.anew;
+
+/**
+ * Created by Nati on 8/14/2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.15.4/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.15.4/android_login_api/register.php";
+}
